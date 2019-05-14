2c8a2df0f1bceaae000596e05bce005d412bf1ef
Trevol <iam.vladimir.tretyak@gmail.com>
2019-05-14 20:02:57 +0300

    testing RndShapes model

--- experiments/RandomShapes/test_rnd_shapes.py
+++ experiments/RandomShapes/test_rnd_shapes.py
@@ -1,14 +1,23 @@
+import numpy as np
+from models.enet import ENet
+from experiments.RandomShapes.RndShapesDataset import RndShapesDataset
+import torch
 
 
 def main():
-    from models.enet import ENet
-    from experiments.RandomShapes.RndShapesDataset import RndShapesDataset
-
     classesNum = len(RndShapesDataset.color_encoding)
     model = ENet(classesNum)
-    import torch
-    state = torch.load('./checkpoints/ENet')
+
+    state = torch.load('./checkpoints/ENet_', map_location='cpu')
     model.load_state_dict(state['state_dict'])
     # load image, resize and convert to mini-batch tensor
+
+    inputMiniBatch = torch.tensor(np.zeros([1, 3, 768, 1024], np.float32))
+    result = model(inputMiniBatch)
+    print(result.dtype, result.shape)
+    result = result.detach().numpy()
+    result = result.argmax(axis=0)
+    print(result.min(), result.max())
+
 
 main()
